1ea84212c42d8c4821553de24ebb212a16e64ad1
mosergary <gary80397@gmail.com>
2019-10-06 11:33:18 -0600

    Add files via upload

--- Collections/src/com/cit360/Main.java
+++ Collections/src/com/cit360/Main.java
@@ -123,6 +123,7 @@ public class Main {
 
         //HashSet
         // ignores duplicates one of the differences between a list and a set
+        // sets are great for ensureing that duplicate entries are not made into an array.  example would be ssn#
 
         HashSet<String> number = new HashSet<String>();
         number.add("1");
@@ -134,9 +135,10 @@ public class Main {
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
-        //TreeSee
+        //TreeSet
         //Maintains ascending order
         //no duplicate it is a Set
+        // this would work well to automatically alpha a list to display does not need addition sorting
         System.out.println("\n"+ "TreeSet"+"\n"+"Maintains order number added at random");
         TreeSet<Integer> tree = new TreeSet<Integer>();
         tree.add(1);
@@ -152,6 +154,111 @@ public class Main {
         System.out.println("\n"+ "added 1 and 6,  one is ignored and 6 is put in order");
         System.out.println(tree);
 
+        // this could be used in a program to sort tasks it will automatically put them in acending order
+        TreeSet<Integer> studentNumber = new TreeSet<Integer>();
+        Scanner myNumber = new Scanner(System.in);
+        System.out.println(" Enter your student number");
+        String inputNumber = myNumber.nextLine();
+        String numberToParse = inputNumber;
+
+        int student = Integer.parseInt(numberToParse);
+        studentNumber.add(student);
+        System.out.println(studentNumber);
+
+
+        //////////////Parse use input to string
+
+
+/////////////////Map/////////////////////////////////////////////////////////////////////////////////////////
+
+////////When using a Map make sure the key is only going to be used once or it will ignore the previous entries
+///the first example show that if you use cities in this example as the key because boise has more
+//than one zip code it will ignore the first enty
+        System.out.println("\n " + "/////////////  Map   //////////////////////////");
+
+        Map<String,Integer> zip = new HashMap<String,Integer>();
+zip.put("boise", 83706);
+zip.put("boise",83702);
+zip.put("Meridian", 83686);
+zip.put("Caldwell",83607);
+
+System.out.println(zip);
+System.out.println(zip.get("boise"));
+
+        Set<Map.Entry<String,Integer>> idaho = zip.entrySet();
+for (Map.Entry<String,Integer> out:idaho ) {
+    System.out.println(out.getKey() + ":" + out.getValue());
+}
+
+/////////the Better way is to use the zip code as the key that way you can call boise multiple time
+// //because it has more than one zip code
+        Map<Integer,String> zip2 = new HashMap<Integer, String>();
+        zip2.put(83706,"boise");
+        zip2.put(83686, "Meridian");
+        zip2.put(83607,"caldwell");
+        zip2.put(83704,"boise");
+
+        System.out.println(zip2);
+
+
+        Set<Map.Entry<Integer,String>>idaho2 = zip2.entrySet();
+        for (Map.Entry<Integer,String> out:idaho2 ) {
+            System.out.println(out.getKey() + ":" + out.getValue());
+        }
+        System.out.println(zip2.get(83706));
+        System.out.println(zip2.get(83704));
+
+//ask user to enter in zip code it will compare zipcode to map and output the city
+
+        Scanner myZip = new Scanner(System.in);
+        System.out.println("Enter in ZipCode 83706, 83686,83607,83704");
+        String userZip = myZip.nextLine();
+        int result = Integer.parseInt(userZip);
+        System.out.println("your zip code is in " + zip2.get(result));
+
+
+///////////////////////////////////       Queue   ////////////////////////////////////
+
+        System.out.println("\n " + "/////////////  Queue   //////////////////////////");
+
+        Queue<String> course = new LinkedList<>();
+        course.add("cit260");
+        course.add("cit360");
+        course.add("cit460");
+        course.add("cit160");
+        System.out.println(course);
+
+        course.add("this is the new added course");
+
+        System.out.println(course);
+        System.out.println("notice is is added to the tail not to the front of the queue");
+
+        course.remove();
+
+        System.out.println(course);
+        System.out.println("notice when you remove it pull the first one away");
+
+        System.out.println(course.peek());
+
+        System.out.println("this one will show them element in the first spot");
+
+        System.out.println(course.size());
+        System.out.println("this shows how many element are currently in your queue");
+
+        //ask user for input tells the user when ther item is in the queue
+        Scanner myQueue = new Scanner(System.in);
+        System.out.println("Enter a class");
+        String userClass = myQueue.nextLine();
+        course.add(userClass);
+        System.out.println(course);
+        System.out.println("Your class is " + course.size() + "th from the beginning");
+
+
+}
+
+
+
+
 
 
 
@@ -164,6 +271,6 @@ public class Main {
 
         }
 
-    }
+
 
 
